8f388e581762b049d6eb147fe1c1c313eb94e4ac
Trixt0r <trixter16@web.de>
2014-03-28 22:03:40 +0100

    transformation test fix

--- SpriterTests/src/com/brashmonkey/spriter/tests/TransformationTest.java
+++ SpriterTests/src/com/brashmonkey/spriter/tests/TransformationTest.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.brashmonkey.spriter.Player;
